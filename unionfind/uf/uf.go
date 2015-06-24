fa152e0af384f4982bf329a8a849e5bda9e5aac4
sephiroth2029 <sephiroth2029@gmail.com>
2015-06-24 14:44:02 -0500

    Implemented the  Quick-Find algorithm.

--- unionfind/uf/uf.go
+++ unionfind/uf/uf.go
@@ -3,18 +3,36 @@ package uf
 // UF is the current Union-Find algorithm. For more details of the purpose of this
 // algorithm check: http://algs4.cs.princeton.edu/15uf/
 type UF struct {
-	N int64
+	N  int
+	id []int
+}
+
+// NewUF creates and initializes a new implementation of the algorithm.
+func NewUF(N int) *UF {
+	var uf UF
+
+	uf.N = N
+	uf.id = make([]int, N)
+	for i := 0; i < N; i++ {
+		uf.id[i] = i
+	}
+
+	return &uf
 }
 
 // Connected checks if two objects are in the same component
-func (*UF) Connected(p, q int64) bool {
-	if p == q {
-		return true
-	}
-	return false
+func (uf *UF) Connected(p, q int) bool {
+	return uf.id[p] == uf.id[q]
 }
 
 // Union connects the given objects.
-func (*UF) Union(p, q int64) {
+func (uf *UF) Union(p, q int) {
+	pid := uf.id[p]
+	qid := uf.id[q]
 
+	for i := range uf.id {
+		if uf.id[i] == pid {
+			uf.id[i] = qid
+		}
+	}
 }
